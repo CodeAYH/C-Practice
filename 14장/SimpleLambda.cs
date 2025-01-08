1df6ae47bfb5f136f672435d9c836e0764394227
yhan <yhan@visionsemicon.co.kr>
2025-01-08 16:06:53 +0900

    Update

---
+++ 14장/SimpleLambda.cs
@@ -0,0 +1,13 @@
+﻿namespace _14장
+{
+    internal class SimpleLambda
+    {
+        delegate int Calculate(int a, int b);
+
+        static void MainSimpleLambda(string[] args)
+        {
+            Calculate calc = (a, b) => a + b;
+            Console.WriteLine(calc(3, 4));
+        }
+    }
+}
---
+++ 14장/StatementLambda.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _14장
+{
+    delegate string Concatenate(string[] args);
+    internal class StatementLambda
+    {
+        static void MainStatementLambda(string[] args)
+        {
+            Concatenate concat =
+                (arr) =>
+                {
+                    string result = "";
+                    foreach (string s in arr)
+                        result += s;
+                    return result;
+                };
+            Console.WriteLine(concat(args));
+        }
+    }
+}
