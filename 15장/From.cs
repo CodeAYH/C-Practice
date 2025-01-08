0e6ddc0f93f9c0a661081c90cb01c73806efdcf5
yhan <yhan@visionsemicon.co.kr>
2025-01-08 16:35:24 +0900

    Update

---
+++ 15장/From.cs
@@ -0,0 +1,18 @@
+﻿using System.Runtime.CompilerServices;
+
+namespace _15장
+{
+    internal class From
+    {
+        static void MainFrom(string[] args)
+        {
+            int[] numbers = { 9, 2, 6, 4, 5, 3, 7, 8, 1, 10 };
+            var result = from n in numbers
+                         where n % 2 == 0
+                         orderby n
+                         select n;
+            foreach (int n in result)
+                Console.WriteLine(n);
+        }
+    }
+}
---
+++ 15장/GroupBy.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _15장
+{
+    internal class GroupBy
+    {
+        static void MainGroupBy()
+        {
+            Profile[] arr =
+            {
+                new Profile() { Name = "정우성", Height = 186 },
+                new Profile() { Name = "김태희", Height = 158 },
+                new Profile() { Name = "고현정", Height = 172 },
+                new Profile() { Name = "이문세", Height = 178 },
+                new Profile() { Name = "하하", Height = 171 },
+            };
+
+            var list = from profile in arr
+                       orderby profile.Height
+                       group profile by profile.Height < 175 into g
+                       select new { GroupKey = g.Key, Profiles = g };
+            foreach (var Group in list)
+            {
+                Console.WriteLine(Group.GroupKey);
+                foreach (var profile in Group.Profiles)
+                {
+                    Console.WriteLine($"{profile.Name} : {profile.Height}");
+                }
+            }
+        }
+    }
+}
